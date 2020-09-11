c98d31dbf0414e1d0dd55d727945324328e98b5e
vpredivo <victor.redivo@loft.com.br>
2020-09-10 22:28:30 -0300

    feat/completed game

--- learning_game.py
+++ learning_game.py
@@ -8,8 +8,9 @@ SCREEN_SIZE = 300
 SURFACE = 10
 SAMPLE_NUMBER = 4
 
-COEF_A = 1 #random.randint(0, 2)
-COEF_B = 0 #random.randint(0, 30)
+COEF_A = random.random()*4
+COEF_B = random.randint(0, 20)
+SIDE = random.random()
 
 def on_grid_random():
     """
@@ -25,18 +26,32 @@ def color_point(rec, revealed):
     """
     create colorful point
     """
-    if revealed == 1:
-        if rec.left*COEF_A + COEF_B <= rec.top:
-            r = 255
-            g = 0
-            b = 0
+    if SIDE < 0.5:
+        if revealed == 1:
+            if rec.left*COEF_A + COEF_B <= rec.top:
+                r = 255
+                g = 0
+                b = 0
+            else:
+                r = 0
+                g = 255
+                b = 255
+            return pygame.draw.rect(screen, (r, g, b), (rec.left, rec.top, 10, 10))
         else:
-            r = 0
-            g = 255
-            b = 255
-        return pygame.draw.rect(screen, (r, g, b), (rec.left, rec.top, 10, 10))
+            return pygame.draw.rect(screen, (255, 255, 255), (rec.left, rec.top, 10, 10))
     else:
-        return pygame.draw.rect(screen, (255, 255, 255), (rec.left, rec.top, 10, 10))
+        if revealed == 1:
+            if rec.left*COEF_A + COEF_B >= rec.top:
+                r = 255
+                g = 0
+                b = 0
+            else:
+                r = 0
+                g = 255
+                b = 255
+            return pygame.draw.rect(screen, (r, g, b), (rec.left, rec.top, 10, 10))
+        else:
+            return pygame.draw.rect(screen, (255, 255, 255), (rec.left, rec.top, 10, 10))
 
 
 def points_reveal():
@@ -80,10 +95,16 @@ for i in range(0, AMOUNT):
     x, y = on_grid_random()
     objectsRect.append(pygame.draw.rect(screen, (255, 255, 255), (x, y, 10, 10)))
     
-    if x*COEF_A + COEF_B <= y:
-        PointsTrueValue.append(1)
+    if SIDE < 0.5:
+        if x*COEF_A + COEF_B <= y:
+            PointsTrueValue.append(1)
+        else:
+            PointsTrueValue.append(0)
     else:
-        PointsTrueValue.append(0)
+        if x*COEF_A + COEF_B >= y:
+            PointsTrueValue.append(1)
+        else:
+            PointsTrueValue.append(0)
     clickedObj.append(0)
     GuessedPoints.append(0)
 
